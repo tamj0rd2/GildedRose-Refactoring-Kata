7d7ada28e115a36a6ebb18df30a32352ff8cc14b
Tamara Jordan <tamara.jordan@springernature.com>
2024-01-03 09:51:31 +0000

    Invert some if statements

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -36,19 +36,19 @@ class GildedRose(var items: List<Item>) {
             }
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.name == "Aged Brie") {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                } else {
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                        item.quality = item.quality - item.quality
+                    } else {
                         if (item.quality > 0) {
                             if (item.name != "Sulfuras, Hand of Ragnaros") {
                                 item.quality = item.quality - 1
                             }
                         }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
                     }
                 }
             }
