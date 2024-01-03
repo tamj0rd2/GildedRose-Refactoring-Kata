b11b6a1e1c3bf2623b00181969d81bb35a63183b
Tamara Jordan <tamara.jordan@springernature.com>
2024-01-03 09:52:34 +0000

    Refactor to use a when

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -36,14 +36,16 @@ class GildedRose(var items: List<Item>) {
             }
 
             if (item.sellIn < 0) {
-                if (item.name == "Aged Brie") {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                when (item.name) {
+                    "Aged Brie" -> {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
                     }
-                } else {
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    "Backstage passes to a TAFKAL80ETC concert" -> {
                         item.quality = item.quality - item.quality
-                    } else {
+                    }
+                    else -> {
                         if (item.quality > 0) {
                             if (item.name != "Sulfuras, Hand of Ragnaros") {
                                 item.quality = item.quality - 1
