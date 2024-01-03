c30d79f9345bc624d62c59bd21e7b3b950cbe3b8
Tamara Jordan <tamara.jordan@springernature.com>
2024-01-03 09:53:52 +0000

    Use plus equals and minus equals

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -8,23 +8,23 @@ class GildedRose(var items: List<Item>) {
             if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (item.quality > 0) {
                     if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
+                        item.quality -= 1
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality += 1
 
                     if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.quality += 1
                             }
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.quality += 1
                             }
                         }
                     }
@@ -39,16 +39,16 @@ class GildedRose(var items: List<Item>) {
                 when (item.name) {
                     "Aged Brie" -> {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1
+                            item.quality += 1
                         }
                     }
                     "Backstage passes to a TAFKAL80ETC concert" -> {
-                        item.quality = item.quality - item.quality
+                        item.quality -= item.quality
                     }
                     else -> {
                         if (item.quality > 0) {
                             if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
+                                item.quality -= 1
                             }
                         }
                     }
