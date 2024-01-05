9631eab793f053b00ef9a3957164f2ea7c65f202
Tamara Jordan <tamara.jordan@springernature.com>
2024-01-05 11:19:36 +0000

    Extract functions for updating brie and backstage passes

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -9,36 +9,41 @@ class GildedRose(var items: List<Item>) {
         }
     }
 
-    private fun Item.update() {
-        if (name != "Aged Brie" && name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (quality > 0) {
-                if (name != "Sulfuras, Hand of Ragnaros") {
-                    quality -= 1
-                }
-            }
-        } else {
+    private fun Item.updateAgedBrie() {
+        if (quality < 50) {
+            quality += 1
+        }
+    }
+
+    private fun Item.updateBackstagePass() {
+        if (quality < 50) {
+            quality += 1
+        }
+
+        if (sellIn < 11) {
             if (quality < 50) {
                 quality += 1
-
-                if (name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality += 1
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality += 1
-                        }
-                    }
-                }
             }
         }
 
-        if (name != "Sulfuras, Hand of Ragnaros") {
-            sellIn -= 1
+        if (sellIn < 6) {
+            if (quality < 50) {
+                quality += 1
+            }
         }
+    }
+
+    private fun Item.update() {
+
+        when (name) {
+            "Aged Brie" -> updateAgedBrie()
+            "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePass()
+            else -> {
+                if (quality > 0 && name != "Sulfuras, Hand of Ragnaros") quality -= 1
+            }
+        }
+
+        if (name != "Sulfuras, Hand of Ragnaros") sellIn -= 1
 
         if (sellIn < 0) {
             when (name) {
