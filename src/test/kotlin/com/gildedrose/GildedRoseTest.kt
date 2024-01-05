91f199359f4541416d8b66f3b7e26f732e257630
Tamara Jordan <tamara.jordan@springernature.com>
2024-01-05 11:22:06 +0000

    Comment out unused test for now

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,16 +1,12 @@
 package com.gildedrose
 
-import org.junit.jupiter.api.Test
-import strikt.api.expectThat
-import strikt.assertions.isEqualTo
-
 internal class GildedRoseTest {
 
-    @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        expectThat(app.items[0].name).isEqualTo("fixme")
-    }
+    //@Test
+    //fun foo() {
+    //    val items = listOf(Item("foo", 0, 0))
+    //    val app = GildedRose(items)
+    //    app.updateQuality()
+    //    expectThat(app.items[0].name).isEqualTo("fixme")
+    //}
 }
