40c239cab98c5880edc4e60a12e3cabc163983a2
Tamara Jordan <tamara.jordan@springernature.com>
2024-01-05 15:34:15 +0000

    Move item extension functions to the Item class

--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -6,3 +6,42 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
 }
+
+fun Item.updateQuality() {
+    when {
+        name == "Aged Brie" -> adjustQualityBasedOnSellByDate(1)
+        name == "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePassQuality()
+        name.startsWith("Conjured") -> adjustQualityBasedOnSellByDate(-2)
+        else -> adjustQualityBasedOnSellByDate(-1)
+    }
+}
+
+private val Item.hasAlreadyReachedSellByDate get() = sellIn <= 0
+
+private fun Item.adjustQualityBy(amount: Int) {
+    val projectedQuality = quality + amount
+
+    quality = when {
+        projectedQuality > GildedRose.MAXIMUM_QUALITY -> GildedRose.MAXIMUM_QUALITY
+        projectedQuality < 0 -> 0
+        else -> projectedQuality
+    }
+}
+
+private fun Item.adjustQualityBasedOnSellByDate(amount: Int) {
+    adjustQualityBy(if (hasAlreadyReachedSellByDate) amount * 2 else amount)
+}
+
+private fun Item.updateBackstagePassQuality() {
+    if (hasAlreadyReachedSellByDate) {
+        quality = 0
+        return
+    }
+
+    val amount = when {
+        sellIn <= 5 -> 3
+        sellIn <= 10 -> 2
+        else -> 1
+    }
+    adjustQualityBy(amount)
+}
