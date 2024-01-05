61a18666e761922d1caf8df49d5a2bc3c9cddf21
Tamara Jordan <tamara.jordan@springernature.com>
2024-01-05 13:38:22 +0000

    Add a function to adjust quality

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -17,20 +17,23 @@ class GildedRose(var items: List<Item>) {
 
     private val Item.isPastSellByDate get() = sellIn < 0
 
+    private fun Item.adjustQualityBy(amount: Int) {
+        quality = min(quality + amount, MAXIMUM_QUALITY)
+    }
+
     private fun Item.updateAgedBrie() {
         sellIn -= 1
 
         val increment = if (isPastSellByDate) 2 else 1
-
-        quality = min(quality + increment, MAXIMUM_QUALITY)
+        adjustQualityBy(increment)
     }
 
     private fun Item.updateBackstagePass() {
-        if (quality < 50) quality += 1
+        if (quality < 50) adjustQualityBy(1)
 
-        if (sellIn < 11 && quality < MAXIMUM_QUALITY) quality += 1
+        if (sellIn < 11 && quality < MAXIMUM_QUALITY) adjustQualityBy(1)
 
-        if (sellIn < 6 && quality < MAXIMUM_QUALITY) quality += 1
+        if (sellIn < 6 && quality < MAXIMUM_QUALITY) adjustQualityBy(1)
 
         sellIn -= 1
 
@@ -43,11 +46,11 @@ class GildedRose(var items: List<Item>) {
             "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePass()
             "Sulfuras, Hand of Ragnaros" -> {}
             else -> {
-                if (quality > 0) quality -= 1
+                if (quality > 0) adjustQualityBy(-1)
 
                 sellIn -= 1
 
-                if (isPastSellByDate && quality > 0) quality -= 1
+                if (isPastSellByDate && quality > 0) adjustQualityBy(-1)
             }
         }
     }
