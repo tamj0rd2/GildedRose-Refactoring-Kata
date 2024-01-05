38a25c827156f29d6bef39c7468ca8caeab4b0d4
Tamara Jordan <tamara.jordan@springernature.com>
2024-01-05 11:14:09 +0000

    Add extension function to update Item

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,51 +5,57 @@ class GildedRose(var items: List<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality -= 1
-                    }
+            item.update()
+        }
+    }
+
+    private fun Item.update() {
+        if (name != "Aged Brie" && name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (quality > 0) {
+                if (name != "Sulfuras, Hand of Ragnaros") {
+                    quality -= 1
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality += 1
+            }
+        } else {
+            if (quality < 50) {
+                quality += 1
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality += 1
-                            }
+                if (name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality += 1
                         }
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality += 1
-                            }
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality += 1
                         }
                     }
                 }
             }
+        }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
-            }
+        if (name != "Sulfuras, Hand of Ragnaros") {
+            sellIn -= 1
+        }
 
-            if (item.sellIn < 0) {
-                when (item.name) {
-                    "Aged Brie" -> {
-                        if (item.quality < 50) {
-                            item.quality += 1
-                        }
+        if (sellIn < 0) {
+            when (name) {
+                "Aged Brie" -> {
+                    if (quality < 50) {
+                        quality += 1
                     }
-                    "Backstage passes to a TAFKAL80ETC concert" -> {
-                        item.quality -= item.quality
-                    }
-                    else -> {
-                        if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality -= 1
-                            }
+                }
+
+                "Backstage passes to a TAFKAL80ETC concert" -> {
+                    quality -= quality
+                }
+
+                else -> {
+                    if (quality > 0) {
+                        if (name != "Sulfuras, Hand of Ragnaros") {
+                            quality -= 1
                         }
                     }
                 }
@@ -57,4 +63,3 @@ class GildedRose(var items: List<Item>) {
         }
     }
 }
-
